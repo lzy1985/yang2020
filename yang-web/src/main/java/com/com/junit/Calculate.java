c0f9b334b435c817c5725de8eeb1b5c264185384
lvzhongyang <lvzhongyang@weiyankeji.cn>
2018-01-11 06:30:46 +0800

    junit

---
+++ yang-web/src/main/java/com/com/junit/Calculate.java
@@ -0,0 +1,15 @@
+package com.com.junit;
+
+/**
+ * @author lzy
+ * @version 1.0.0
+ * @date 2018/1/11 上午5:54
+ * @Description
+ */
+public class Calculate {
+
+    public Integer sum(Integer first,Integer second){
+        System.out.println("Adding values:"+first+"+"+second);
+        return first+second;
+    }
+}
