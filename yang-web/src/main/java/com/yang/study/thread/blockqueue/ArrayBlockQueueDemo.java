30e5cafed61be580fdf0dbd23c771d73661dab5e
lvzhongyang <lvzhongyang@weiyankeji.cn>
2018-01-15 06:02:03 +0800

    blockqueue

--- yang-web/src/main/java/com/yang/study/thread/blockqueue/ArrayBlockQueueDemo.java
+++ yang-web/src/main/java/com/yang/study/thread/blockqueue/ArrayBlockQueueDemo.java
@@ -21,3 +21,47 @@ public class ArrayBlockQueueDemo {
 
     }
 }
+
+
+class Product extends Thread{
+
+    private ArrayBlockingQueue<String> queue;
+
+    public Product(ArrayBlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    public void run(){
+        try {
+            String[] s = new String[]{"老虎","狮子","大象","狼"};
+            for (String s1 : s) {
+                queue.put(s1);
+                System.out.println("生产出玩具："+s1);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Consumer extends Thread{
+
+    private ArrayBlockingQueue<String> queue;
+
+    public Consumer(ArrayBlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+
+    public void run(){
+        try {
+            for (int i = 0; i < 4; i++) {
+                System.out.println("消费玩具："+queue.take());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
