6e1118c932e4f7c5fc7e7c56e4f354dbe8d4bdb8
zy <zhongyang_1985@163.com>
2020-06-25 23:35:45 +0800

    test

---
+++ yang-web/src/main/java/com/yang/advice/Azy.java
@@ -0,0 +1,14 @@
+package com.yang.advice;
+
+import lombok.Data;
+
+/**
+ * @author lzy
+ * @version 1.0.0
+ * @date 2020/6/25 下午11:34
+ * @Description
+ */
+@Data
+public class Azy {
+    private String aa;
+}
